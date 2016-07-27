e16b7793375bda580ad4f005c83f2d8901febdfa
yangli_sky@163.com <yangli_sky@163.com>
2016-07-27 12:11:46 +0800

    a

--- 01Easy.Log/App_Start/FilterConfig.cs
+++ 01Easy.Log/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using Easy.Log.Utility;
+using System.Web;
 using System.Web.Mvc;
 
 namespace _01Easy.Log
@@ -7,7 +8,8 @@ namespace _01Easy.Log
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorAttribute());
         }
     }
 }
---
+++ 01Easy.Log/Utility/ErrorAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Easy.Public.MyLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Easy.Log.Utility
+{
+    public class ErrorAttribute : FilterAttribute, IExceptionFilter
+    {
+        /// <summary>
+        /// 错误处理
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public void OnException(ExceptionContext filterContext)
+        {
+            var url = filterContext.HttpContext.Request.Url.AbsolutePath;
+            var ex = filterContext.Exception;
+            var request = filterContext.HttpContext.Request;
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in filterContext.HttpContext.Request.Form.AllKeys)
+            {
+                sb.AppendFormat("{0}:{1}", item, request.Form[item]);
+                sb.AppendLine();
+            }
+            sb.AppendLine(ex.Message);
+            sb.AppendLine(ex.StackTrace);
+
+            LogManager.Error(url, sb.ToString());
+
+            filterContext.ExceptionHandled = true;
+            filterContext.Result = new JsonResult() { Data = "服务器错误", JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+    }
+}
